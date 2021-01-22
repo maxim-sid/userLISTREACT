a5c10de1c5ec0d61ff747c86e87c013264f53173
maxim-sid <whramiii@gmail.com>
2021-01-22 18:59:31 +0200

    init 2

---
+++ src/components/UserItem/UserItem.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import styles from './UserItem.module.scss';
+
+export default class UserItem extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  deleteHandler = () => {
+    this.props.deleteUser(this.props.cell);
+  };
+
+  render() {
+    const {
+      name,
+      location,
+      phone,
+      email,
+      cell,
+      picture: { medium: imgSrc },
+    } = this.props;
+
+    return (
+      <div className={styles.userItemContainer}>
+        <img className={styles.userAvatar} src={imgSrc} />
+        <div>{`${name.first} ${name.last}`}</div>
+        <div>{`Telephone number: ${phone}`}</div>
+        <div>{`${location.country}, ${location.city}`}</div>
+        <div className={styles.deleteButton} onClick={this.deleteHandler}>DELETE</div>
+      </div>
+    );
+  }
+}
